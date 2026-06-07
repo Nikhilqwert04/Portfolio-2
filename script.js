3c1f926e5c5a628210481b6c3cf177e78ec0047b
Nikhilqwert04 <nikhilluthra04052007@gmail.com>
2026-06-07 21:54:59 +0530

    refactor: migrate hover styles from HTML to script-based dynamic class assignment

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-let test = document.querySelector(".sample1");
-test.addEventListener("mouseover",function(){
-test.classList,add("text-green");
-})
+let tests = document.querySelectorAll(".sample1");
+
+tests.forEach((test)=> {
+    test.addEventListener("mouseover", function(){
+        test.classList.add(
+            "transition-all",
+            "duration-150",
+            "hover:border-b-2",
+            "border-[#cfff45]",
+            "hover:text-white",
+            "hover:text-lg",
+        );
+    });
+});
