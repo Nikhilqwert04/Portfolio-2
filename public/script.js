d9f857d7d5a3d8ffe5e732534699da5264f6d991
Nikhilqwert04 <nikhilluthra04052007@gmail.com>
2026-06-08 22:41:07 +0530

    feat: implement responsive mobile navigation menu with custom toggle animations

--- public/script.js
+++ public/script.js
@@ -1,5 +1,41 @@
-let cursor = document.querySelector(".cursordelay");
-window.addEventListener("mousemove",function(detail){
-    cursor.style.top=detail.clientY-23+"px";
-    cursor.style.left=detail.clientX-23+"px";
+let cursor = document.querySelector(".cursordelay")
+
+window.addEventListener("mousemove", function (detail) {
+    cursor.style.top = detail.clientY - 23 + "px";
+    cursor.style.left = detail.clientX - 23 + "px";
+})
+
+let navbar = document.querySelector("#manv");
+let icon = document.querySelector(".nav");
+let NavTexts = document.querySelectorAll("#main a")
+
+let isOpen = false;
+icon.addEventListener("click", function () {
+    if (!isOpen) {
+        navbar.style.animation =
+            "mobileMenuOpen .6s ease forwards";
+        setTimeout(function () {
+            NavTexts.forEach(function (NavText) {
+                NavText.classList.remove("hidden")
+            })
+        }, 300)
+    } 
+    else {
+        navbar.style.animation =
+            "mobileMenuClose .6s ease forwards";
+        NavTexts.forEach(function (NavText) {
+            NavText.classList.add("hidden")
+        })
+    }
+    isOpen = !isOpen;
+})
+NavTexts.forEach(function (NavText) {
+    NavText.addEventListener("click", function () {
+        navbar.style.animation =
+            "mobileMenuClose .6s ease forwards";
+        NavTexts.forEach(function (link) {
+            link.classList.add("hidden")
+        });
+        isOpen = false;
+    })
 })
